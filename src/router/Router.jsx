a30473c4847a07a880963b0c88e41a18d52799c0
rafittu <rafittu@outlook.com>
2024-02-09 16:42:01 -0300

    feat: user login page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 function Router() {
   return (
     <Routes>
+      <Route exact path="/login" element={<Login />} />
       <Route exact path="/" element={<Home />} />
     </Routes>
   );
